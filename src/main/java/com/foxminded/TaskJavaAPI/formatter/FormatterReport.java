0d4ad4e927b2eca6082aee358a89ccfaab32dfc2
Maxim Sheyko <maxim37377373@gmail.com>
2021-08-20 12:55:07 +0400

    add method createLineTable

--- src/main/java/com/foxminded/TaskJavaAPI/formatter/FormatterReport.java
+++ src/main/java/com/foxminded/TaskJavaAPI/formatter/FormatterReport.java
@@ -7,18 +7,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class FormatterReport {
 
-    public static final String ILLEGAL_EX = "list is null";
+    public static final String ILLEGAL_EXCEPTION = "list is null";
     public static final String LINE_SEPARATOR = System.lineSeparator();
     public static final String VERTICAL_LINE = "|";
 
-    public String getForm(List<Racer> racers) throws IllegalAccessException {
+    public String getForm(List<Racer> racers) throws IllegalArgumentException {
 	if (racers == null) {
-	    throw new IllegalAccessException(ILLEGAL_EX);
+	    throw new IllegalArgumentException(ILLEGAL_EXCEPTION);
 	}
 
 	StringBuilder report = new StringBuilder();
-	int maxLengthFieldName = getMaxLengthLine(racers.stream().map(Racer::getName).toList());
-	int maxLengthFieldCar = getMaxLengthLine(racers.stream().map(Racer::getCar).toList());
+	int maxLengthFieldName = getMaxLengthLine(racers.stream()
+		.map(Racer::getName)
+		.toList());
+	int maxLengthFieldCar = getMaxLengthLine(racers.stream()
+		.map(Racer::getCar)
+		.toList());
 	int topRacer = 15;
 	AtomicInteger position = new AtomicInteger(1);
 
@@ -26,20 +30,25 @@ public class FormatterReport {
 	racers.stream()
 		.forEach(racer -> racer.setTime(racer.getTime().replace("PT", "").replace("M", ":").replace("S", "")));
 	racers.stream().limit(topRacer)
-		.forEach(racer -> report.append(String.format("%-2d ", position.getAndIncrement()))
-			.append(String.format("%" + -maxLengthFieldName + "s", racer.getName()))
-			.append(VERTICAL_LINE)
-			.append(String.format("%" + -maxLengthFieldCar + "s|%s%s", racer.getCar(), racer.getTime(),
-				LINE_SEPARATOR)));
+		.forEach(racer -> report.append(createLineTable(racer, maxLengthFieldCar, maxLengthFieldName, position)));
 	report.append(String.format("%s", "-".repeat(maxLengthFieldCar + maxLengthFieldName) + LINE_SEPARATOR));
 	racers.stream().skip(topRacer)
-		.forEach(racer -> report.append(String.format("%-2d ", position.getAndIncrement()))
-			.append(String.format("%" + -maxLengthFieldName + "s", racer.getName()))
-			.append(VERTICAL_LINE)
-			.append(String.format("%" + -maxLengthFieldCar + "s|%s%s", racer.getCar(), racer.getTime(),
-				LINE_SEPARATOR)));
+		.forEach(racer ->report.append(createLineTable(racer, maxLengthFieldCar, maxLengthFieldName, position)));
 
 	return report.toString();
+    }
+    
+    private String createLineTable(Racer racer,int maxLengthFieldCar,int maxLengthFieldName, AtomicInteger position) {
+	StringBuilder line = new StringBuilder();
+	
+	line.append(String.format("%-2d ", position.getAndIncrement()))
+	.append(String.format("%" + -maxLengthFieldName + "s", racer.getName()))
+	.append(VERTICAL_LINE)
+	.append(String.format("%" + -maxLengthFieldCar + "s|%s%s", racer.getCar(),racer.getTime(),
+		LINE_SEPARATOR));
+	
+	return line.toString();
+	
     }
 
     private int getMaxLengthLine(List<String> lines) {
